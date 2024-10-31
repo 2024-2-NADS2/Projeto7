81c759df3ba8fd8a17f2f3834b215a8d418028dd
karollemos06 <163611155+karollemos06@users.noreply.github.com>
2024-10-30 22:03:49 -0300

    Create GuiaPratico.js

---
+++ src/Frontend/cgkm HTML/src/pages/GuiaPratico.js
@@ -0,0 +1,15 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
+
+
+function GuiaPratico(){
+    return(
+        <>
+        <Header/>
+        <Footer/>
+        </>
+        )
+}
+
+export default GuiaPratico;

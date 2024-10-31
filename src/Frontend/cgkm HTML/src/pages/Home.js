afee29ce86b609b7b0959280c975337f893b8d67
Orlandi05 <163611471+Orlandi05@users.noreply.github.com>
2024-10-30 22:08:25 -0300

    Create Home.js

---
+++ src/Frontend/cgkm HTML/src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Slides from '../Components/Slides'
+import Info from '../Components/Info'
+import MainContainer from '../Components/MainContainer'
+import Footer from "../Components/Footer"
+
+function Home() {
+
+    
+    return (
+        <>
+
+        <Header/>
+        <Slides/>
+        <Info/>
+        <MainContainer/>
+        <Footer/>
+
+        </>
+    )
+}
+
+export default Home

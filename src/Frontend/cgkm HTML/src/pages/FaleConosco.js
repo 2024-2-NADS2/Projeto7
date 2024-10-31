1e0417336ebf1a42bf334af108db80cfd2467d4d
karollemos06 <163611155+karollemos06@users.noreply.github.com>
2024-10-30 22:04:14 -0300

    Create FaleConosco.js

---
+++ src/Frontend/cgkm HTML/src/pages/FaleConosco.js
@@ -0,0 +1,18 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import Fale from "../Components/Fale"
+
+
+
+
+function FaleConosco(){
+    return(
+        <>
+        <Header/>
+        <Fale/>
+        <Footer/>
+        </>
+        )
+}
+
+export default FaleConosco;

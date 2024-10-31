bdc03ac37a091e4300bb38d36f5863247f2a94e3
msantoroo <163611185+msantoroo@users.noreply.github.com>
2024-10-30 21:37:09 -0300

    Create Sobre.js

---
+++ src/Frontend/cgkm HTML/src/pages/Sobre.js
@@ -0,0 +1,17 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import SobreNos from "../Components/SobreNos";
+
+
+
+function Sobre(){
+    return(
+        <>
+        <Header/>
+        <SobreNos/>
+        <Footer/>
+        </>
+        )
+}
+
+export default Sobre;

7c496e7fa09a53f2b8969107045bf2ca76785b0b
Orlandi05 <163611471+Orlandi05@users.noreply.github.com>
2024-10-30 22:07:35 -0300

    Create Calculadora.js

---
+++ src/Frontend/cgkm HTML/src/pages/Calculadora.js
@@ -0,0 +1,15 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
+
+
+function Calculadora(){
+    return(
+        <>
+        <Header/>
+        <Footer/>
+        </>
+        )
+}
+
+export default Calculadora;

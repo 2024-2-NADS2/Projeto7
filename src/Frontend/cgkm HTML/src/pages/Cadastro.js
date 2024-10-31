9612f266688825cae36463ad7924ec2341bebc28
Orlandi05 <163611471+Orlandi05@users.noreply.github.com>
2024-10-30 22:07:20 -0300

    Create Cadastro.js

---
+++ src/Frontend/cgkm HTML/src/pages/Cadastro.js
@@ -0,0 +1,18 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import Cadastrar from "../Components/Cadastrar"
+
+
+
+
+function Cadastro(){
+    return(
+        <>
+        <Header/>
+        <Cadastrar/>
+       
+        </>
+        )
+}
+
+export default Cadastro;

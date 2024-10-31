961b98d1b5efc6723a4bf8cf95452fd2e9b3fd14
caua william <163611437+caua-william@users.noreply.github.com>
2024-10-30 21:46:18 -0300

    Create Agricultura.js

---
+++ src/Frontend/cgkm HTML/src/pages/Agricultura.js
@@ -0,0 +1,17 @@
+import AgriculturaContainer from "../Components/AgriculturaContainer";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
+
+
+function Agricultura(){
+    return(
+        <>
+        <Header/>
+        <AgriculturaContainer/>
+        <Footer/>
+        </>
+        )
+}
+
+export default Agricultura;
